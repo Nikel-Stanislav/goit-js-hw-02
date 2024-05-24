70f4acacded722a8a9fac2273a67b042345ca479
Nikel Stanislav <162141430+Nikel-Stanislav@users.noreply.github.com>
2024-05-25 01:33:26 +0300

    task-1,2,3

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+const spam = "spam";
+const sale = "sale";
+
+function checkForSpam(message) {
+  const blacklistedWord1 = spam;
+  const blacklistedWord2 = sale;
+  const toLowerCaseMessage = message.toLowerCase();
+  const result =
+    toLowerCaseMessage.includes(blacklistedWord1) ||
+    toLowerCaseMessage.includes(blacklistedWord2);
+  return result;
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
